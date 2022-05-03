689ad42d390817fe6f4348e59d4101c159412262
Aditya Menon <adit.gmenon96@gmail.com>
2022-05-03 18:08:39 +0530

    feat: Uploading Executable Asset to Release

--- release.config.js
+++ release.config.js
@@ -4,6 +4,10 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
+        '@semantic-release/github',
+        ["@semantic-release/git", {
+            "assets": ["dist/network_scanner"]
+            // "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+          }]
     ]
 }
